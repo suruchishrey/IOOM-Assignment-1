13b71003258bdf5161c431bda61ab78a0b580907
suruchishrey <57124147+suruchishrey@users.noreply.github.com>
2020-09-28 03:18:22 +0530

    Update Faculty.h

--- Faculty.h
+++ Faculty.h
@@ -15,19 +15,20 @@ class Faculty:public Person
         const int F_ID;
         Designation desig;
         int numCourses;
-        char*courses[SIZE];
+        char courses[SIZE][SIZE];
     public:
         //Explicit call to the constructor of Person class,initialization list
         Faculty(char*name_, Department dept_,Designation des, char*housenum, char*streetname,char*cityname):Person(name_,dept_,housenum,streetname,cityname),F_ID(getID()+4)
         {
+            //cout<<"Faculty constructed->";
             desig=des;
             numCourses=0;
         }
         int getId();
-        bool getCourses(char*course);
-        bool addCourses(char*course);
-        bool removeCourses(char*course);
-        virtual void print();
+        bool getCourses(char course[SIZE]);
+        bool addCourses(char course[SIZE]);
+        bool removeCourses(char course[SIZE]);
+        virtual void print();                       //function overriding
 };
 
 #endif
